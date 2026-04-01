c73e6e22322254e632e433e72ef9cb3b093d1356
Vitin Phum <vitinphum260@gmail.com>
2026-04-01 22:00:27 +0700

    first commit

---
+++ Basic/01_Input_Output.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a, b;
+    cout <<"Enter one number: "; cin >> a;
+    cout <<"Enter one number again: "; cin>>b;
+    int sum = a*b;
+    cout <<"The Result is: "<< sum <<endl;
+    return 0;
+    int myNumber; // camel case
+    int my_number; // snake case 
+    const int MY_NUMBER = 29; // UPPER CASE
+}
+    int myNumber(){
+        return 0;
+    };
+    class EnglishClass{ //  class use Pascal Case
+
+    };
---
+++ Basic/02_Variable.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string name;
+    cout<<"Hello world!"<<endl;
+    cout<<"Enter your name: "; cin>> name;
+    cout<<"My name is: "<< name << endl;
+}
